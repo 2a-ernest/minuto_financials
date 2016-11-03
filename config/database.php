916c9632bb1b3429cec51fc22a7cfeca01146fbb
2a-ernest <ea.asiedu@st.vvu.edu.gh>
2016-11-03 23:32:49 +0000

    changes to config db

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'DB_TYPE'),
+    'default' => env('DB_CONNECTION', 'psgsql'),
 
     /*
     |--------------------------------------------------------------------------
