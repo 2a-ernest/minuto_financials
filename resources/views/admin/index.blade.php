e91d2e75753066e5de0898b78deedcd5218a0588
2a-ernest <ea.asiedu@st.vvu.edu.gh>
2016-11-03 19:31:21 +0000

    changes to year displayed inside admin/index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -191,7 +191,7 @@
 
                 <div class="footer">
                     <div class="footer-left">
-                        <span>&copy; 2013. Shamcey Admin Template. All Rights Reserved.</span>
+                        <span>&copy; 2016. Shamcey Admin Template. All Rights Reserved.</span>
                     </div>
                     <div class="footer-right">
                         <span>Designed by: <a href="http://themepixels.com/">ThemePixels</a></span>
