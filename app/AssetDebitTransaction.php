124a8e14eb710d0b46814b646ab8fdf0927b50e6
nanaoss123 <osei.sekyere@st.vvu.edu.gh>
2016-11-22 15:17:23 +0000

    Task for model creation by Nana and local scoping by Sarah and light mod by Fine Boi completed

---
+++ app/AssetDebitTransaction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AssetDebitTransaction extends Model
+{
+   -------------------
+/**
+    * The "booting" method of the model.
+    *
+    * @return void
+    */
+   protected static function boot()
+   {
+       parent::boot();
+
+       static::addGlobalScope('scope', function(Builder $builder) {
+           $builder->where('param', '>', 'value');
+       });
+   }
+-------------------
+ //
+}
